dd6dfdc3c16ef7e2fee25e05b80eb107133af1aa
Alex Dorey <dorey415@gmail.com>
2013-11-15 16:23:00 -0800

    pushing change to test heroku

--- dkobo/formbuilder/views.py
+++ dkobo/formbuilder/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render_to_response
 
 def main(self):
     return render_to_response("main.haml")
+
+def convert(self, csv_txt):
+    return render_to_response("main.haml")
