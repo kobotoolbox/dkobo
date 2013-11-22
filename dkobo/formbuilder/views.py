dcf3c35ce503ce0e97a7ad282d6e648bce2abca3
Nicolas Straub <nicostraub@gmail.com>
2013-11-22 00:34:50 -0300

    KF-61: csv to xml server side code

--- dkobo/formbuilder/views.py
+++ dkobo/formbuilder/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, HttpResponse
+from django.core.context_processors import csrf
+from django.views.decorators.csrf import ensure_csrf_cookie, csrf_exempt
+
+import utils
 
 def main(self):
     return render_to_response("main.haml")
+
+def csv_to_xform(request, template):
+#	return render_to_response(template, { "data": "it works!", "csv": request, "template": template })
+    csv_data = request.POST
+    
+    survey = utils.create_survey_from_csv_text(csv_data.keys()[0])
+
+    return HttpResponse(survey.to_xml())
+
+@ensure_csrf_cookie
+def sandbox(request):
+	return render_to_response("sandbox.html")
--- dkobo/urls.py
+++ dkobo/urls.py
@@ -6,4 +6,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', 'dkobo.formbuilder.views.main', name='fb'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^csv$', 'dkobo.formbuilder.views.csv_to_xform', { "template": "sandbox-stub.html" }),
+    url(r'^sandbox$', 'dkobo.formbuilder.views.sandbox')
 )
