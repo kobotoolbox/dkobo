126bf8e2741050c8fc662ff5234cd0e7d673644c
Nicolas Straub <nicostraub@gmail.com>
2013-11-22 00:54:18 -0300

    KF-61: refactoring

--- dkobo/formbuilder/views.py
+++ dkobo/formbuilder/views.py
@@ -1,20 +1,13 @@
 from django.shortcuts import render_to_response, HttpResponse
 from django.core.context_processors import csrf
-from django.views.decorators.csrf import ensure_csrf_cookie, csrf_exempt
-
+from django.views.decorators.csrf import ensure_csrf_cookie
 import utils
 
 def main(self):
     return render_to_response("main.haml")
 
-def csv_to_xform(request, template):
-#	return render_to_response(template, { "data": "it works!", "csv": request, "template": template })
+def csv_to_xform(request):
     csv_data = request.POST
-    
     survey = utils.create_survey_from_csv_text(csv_data.keys()[0])
 
     return HttpResponse(survey.to_xml())
-
-@ensure_csrf_cookie
-def sandbox(request):
-	return render_to_response("sandbox.html")
