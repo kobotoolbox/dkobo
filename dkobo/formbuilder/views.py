084598b05161eaa2aafac861543c2d578f4a671f
Alex Dorey <dorey415@gmail.com>
2013-11-25 14:00:33 -0800

    removing whats left of the formbuilder app (try 2)

--- dkobo/formbuilder/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render_to_response
-
-def main(self):
-    return render_to_response("main.haml")
-
-def convert(self, csv_txt):
-    return render_to_response("main.haml")
