c1baf72a2a276b04b8bf3840582aaff6b8c431e6
Nicolas Straub <nicostraub@gmail.com>
2013-11-26 14:54:39 -0300

    KF-88 Integration Test

--- dkobo/formbuilder/tests.py
+++ dkobo/formbuilder/tests.py
@@ -1,14 +1,12 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 from lxml import etree
 
 import pyxform
 
 import utils
 
-class CreateSurveyFromCsvTextTests(TestCase):
-    
-    def test_parses_survey_passed_in_as_csv_and_returns_xml_representation(self):
-        text = """"survey",,,,,
+text = """"survey",,,,,
                 ,"name","type","label","hint","required"
                 ,"gps","geopoint","Record your current location",,"false"
                 ,"start","start",,,
@@ -17,6 +15,18 @@ class CreateSurveyFromCsvTextTests(TestCase):
                 ,"form_title"
                 ,"New survey" """
 
+class CreateSurveyFromCsvTextTests(TestCase):
+    
+    def test_parses_survey_passed_in_as_csv_and_returns_xml_representation(self):
+        
+
         xml = utils.create_survey_from_csv_text(text).to_xml()
         
         etree.fromstring(xml)
+
+class Views_CsvToXformTests(TestCase):
+
+    def test_parses_passed_csv_data(self):
+        response = self.client.post('/csv', {'data': text})
+
+        etree.fromstring(response.content)
