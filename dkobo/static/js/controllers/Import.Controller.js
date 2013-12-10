8cf1d190d002966726e30e6fad564061359508a4
Nicolas Straub <nicostraub@gmail.com>
2013-12-10 11:10:50 -0300

    cleaning up angular files

--- dkobo/static/js/controllers/Import.Controller.js
+++
@@ -1,5 +0,0 @@
-function ImportController($scope, $rootScope, $cookies) {
-    $rootScope.canAddNew = false;
-    $rootScope.activeTab = 'Import CSV';
-    $scope.csrfToken = $cookies.csrftoken;
-}
--- dkobo/static/js/controllers/TopLevelMenu.Controller.js
+++
@@ -1,4 +0,0 @@
-function TopLevelMenuController($scope) {
-    $scope.user_name = 'Chris Nager';
-    $scope.user_avatar = 'chris_nager.jpg';
-}
