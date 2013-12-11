1bea8ca50fdb552dfaf1362975cba2fa5d149e1b
Alex Dorey <dorey415@gmail.com>
2013-12-11 15:07:18 -0800

    fix to get KF-38 working

--- dkobo/koboform/static/js/controllers/Builder.Controller.js
+++ dkobo/koboform/static/js/controllers/Builder.Controller.js
@@ -1,6 +1,3 @@
 function BuilderController($scope, $rootScope) {
-    $scope.startFromScratch = function() {
-	    $scope.xlfSurvey = new XLF.Survey();
-    }
     $rootScope.activeTab = 'Forms';
 }
--- dkobo/koboform/static/js/directives/Builder.Directive.js
+++ dkobo/koboform/static/js/directives/Builder.Directive.js
@@ -1,6 +1,5 @@
 function BuilderDirective($rootScope) {
     return {
-        templateUrl: staticFilesUri + 'templates/Builder.Template.html',
         link: function(scope, element, attrs){
             new SurveyTemplateApp({el: element, survey: scope.xlfSurvey}).render();
         }
