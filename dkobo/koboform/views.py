c57fde0936ed1fe9c991c71362a0d269ae677e3c
Alex Dorey <dorey415@gmail.com>
2013-12-12 16:54:57 -0800

    removing examples

--- dkobo/koboform/views.py
+++ dkobo/koboform/views.py
@@ -22,40 +22,8 @@ def spa(request):
     return render_to_response("index.html")
 
 def list_forms_in_library(request):
-	'''
-	This is a placeholder for the accessor of surveys
-	in the question library.
-	'''
-	return HttpResponse(json.dumps([{
-            'title': 'Safety and Security',
-            'info': 'Last modified yesterday at 5:03pm by Leroy Jenkins',
-            'icon': 'fa-file-text-o',
-            'iconBgColor': 'teal',
-            'tag1': 'Advanced',
-            'tag2': ''
-        },
-        {
-            'title': 'Educational Resources',
-            'info': 'Last modified yesterday at 1:42pm by Rod Stewart',
-            'icon': 'fa-file-text-o',
-            'iconBgColor': 'teal',
-            'tag1': '',
-            'tag2': ''
-        },
-        {
-            'title': 'Food Supply Near Water Supply',
-            'info': 'Last modified yesterday at 11:29am by Cat Stevens',
-            'icon': 'fa-file-text-o',
-            'iconBgColor': 'teal',
-            'tag1': 'Demographics',
-            'tag2': 'Basic Questions'
-        },
-        {
-            'title': 'Local Water Supply Survey',
-            'info': 'Last modified yesterday at 8:50am by Katt Williams',
-            'icon': 'fa-file-text-o',
-            'iconBgColor': 'teal',
-            'tag1': 'Demographics',
-            'tag2': ''
-        }
-    ]))
+    '''
+    This is a placeholder for the accessor of surveys
+    in the question library.
+    '''
+    return HttpResponse("[]")
