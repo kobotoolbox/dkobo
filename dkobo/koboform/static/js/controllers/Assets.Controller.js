ebc54504a559fc7700634513286f17ab70e80631
Nicolas Straub <nicostraub@gmail.com>
2013-12-20 11:13:08 -0300

    Controllers at 100% coverage

--- dkobo/koboform/static/js/controllers/Assets.Controller.js
+++ dkobo/koboform/static/js/controllers/Assets.Controller.js
@@ -1,19 +1,8 @@
 function AssetsController($scope, $rootScope, $resource) {
   var assets = $resource('/question_library_forms/');
 
-  assets.get(function (result) {
-    $scope.infoListItems = $scope.originalListItems = result.list;
-  });
-  $scope.filterList = function(criteria) {
-    $scope.infoListItems = _.filter($scope.originalListItems, function (item) {
-      item.title.indexOf(criteria) > -1 || item.info.indexOf(criteria) > -1;
-    });
-  }
-
-  $scope.removeTags = function () {
-    $( '.info-list-item__tag:empty' ).remove();
-  }
-
+  $scope.infoListItems = assets.get();
+  
   $rootScope.canAddNew = true;
   $rootScope.activeTab = 'Assets';
 }
