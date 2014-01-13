fd9b4a8d721be383df93684a51499dd2942d239c
Nicolas Straub <nicostraub@gmail.com>
2014-01-13 12:06:06 -0300

    #245 - Working. One test isn't passing though... need to refactor the event.preventDefault() invocation out into a service

--- dkobo/koboform/static/js/controllers/Builder.Controller.js
+++ dkobo/koboform/static/js/controllers/Builder.Controller.js
@@ -1,7 +1,14 @@
 /*exported BuilderController*/
 'use strict';
 
-function BuilderController($scope, $rootScope, $routeParams) {
+function BuilderController($scope, $rootScope, $routeParams, $miscUtils) {
     $rootScope.activeTab = 'Forms';
     $scope.routeParams = $routeParams;
+    $rootScope.deregisterLocationChangeStart = $rootScope.$on('$locationChangeStart', function (event) {
+        if ($miscUtils.confirm('Are you sure you want to leave? you will loose any unsaved changes.')){
+            $rootScope.deregisterLocationChangeStart();
+        } else {
+            event.preventDefault();
+        }
+    });
 }
