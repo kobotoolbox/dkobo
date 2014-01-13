215fcf1fffa349255e3ce5f4cd66de397cbd423b
Nicolas Straub <nicostraub@gmail.com>
2014-01-13 16:34:47 -0300

    #248 - added onbeforeunload event

--- dkobo/koboform/static/js/controllers/Builder.Controller.js
+++ dkobo/koboform/static/js/controllers/Builder.Controller.js
@@ -4,11 +4,15 @@
 function BuilderController($scope, $rootScope, $routeParams, $miscUtils) {
     $rootScope.activeTab = 'Forms';
     $scope.routeParams = $routeParams;
-    $rootScope.deregisterLocationChangeStart = $rootScope.$on('$locationChangeStart', function (event) {
+    $rootScope.deregisterLocationChangeStart = $rootScope.$on('$locationChangeStart', handleUnload);
+    function handleUnload(event) {
         if ($miscUtils.confirm('Are you sure you want to leave? you will loose any unsaved changes.')){
             $rootScope.deregisterLocationChangeStart();
         } else {
             $miscUtils.preventDefault(event);
         }
+    }
+    $(window).bind('beforeunload', function(){
+        return 'Are you sure you want to leave?';
     });
 }
