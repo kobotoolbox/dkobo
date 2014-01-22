4d81bfeeec2fb3cf1f225f8b5c0d3f8b0c5ee070
Nicolas Straub <nicostraub@gmail.com>
2014-01-22 12:26:04 -0300

    #411 - display new survey draft in list and #439 - give the user feedback on error

--- dkobo/koboform/static/js/services/MiscUtils.Service.js
+++ dkobo/koboform/static/js/services/MiscUtils.Service.js
@@ -1,11 +1,45 @@
 /*exported MiscUtilsService*/
+/*global $*/
+/*global log*/
 'use strict';
 
 function MiscUtilsService() {
+    var _this = this;
+
     this.confirm = function (message) {
         return confirm(message);
     };
     this.preventDefault = function (event) {
         event.preventDefault();
+    };
+
+    this.bootstrapFileUploader = function ($scope) {
+        $(function(){
+            $('.btn--header-import').eq(0).fileupload({
+                headers: {
+                    "X-CSRFToken": $('meta[name="csrf-token"]').attr('content')
+                },
+                add: function (e, data) {
+                    // maybe display some feedback saying the upload is starting...
+                    log(data.files[0].name + " is uploading...");
+                    data.submit().success(function(result){
+                        window.importedSurveyDraft = JSON.parse(result);
+                        $scope.updateFormList = true;
+                        $scope.$apply();
+                    })
+                    .error(function (result) {
+                        _this.handleXhrError(result);
+                    });
+                }
+            });
+        });
+    };
+
+    this.alert = function (message) {
+        alert(message);
+    };
+
+    this.handleXhrError = function (xhrResult) {
+        _this.alert('The server encountered an error: ' + xhrResult.status + ": " + xhrResult.statusText);
     };
 }
