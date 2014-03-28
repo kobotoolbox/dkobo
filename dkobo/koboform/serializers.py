80975def97ac350aa922acc711ed787760e4cdbb
Alex Dorey <dorey415@gmail.com>
2014-03-28 11:13:16 -0700

    including survey_draft.summary in the serializer

--- dkobo/koboform/serializers.py
+++ dkobo/koboform/serializers.py
@@ -2,11 +2,15 @@ from rest_framework import serializers
 from models import SurveyDraft
 
 class ListSurveyDraftSerializer(serializers.HyperlinkedModelSerializer):
+
     class Meta:
         model = SurveyDraft
-        fields = ('id', 'name', 'asset_type', 'description', 'date_modified')
+        fields = ('id', 'name', 'asset_type', 'summary', 'description', 'date_modified')
+        exclude = ('asset_type', )
 
 class DetailSurveyDraftSerializer(serializers.HyperlinkedModelSerializer):
+
     class Meta:
         model = SurveyDraft
-        fields = ('id', 'name', 'body', 'description', 'date_modified')
+        fields = ('id', 'name', 'body', 'summary', 'description', 'date_modified')
+        exclude = ('asset_type', )
