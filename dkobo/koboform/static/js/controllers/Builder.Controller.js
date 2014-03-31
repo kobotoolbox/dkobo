d2ff4fea174bff516e475235e4567ef60aad5eaa
Nicolas Straub <nicostraub@gmail.com>
2014-03-31 18:43:06 -0300

    #702/#703

--- dkobo/koboform/static/js/controllers/Builder.Controller.js
+++ dkobo/koboform/static/js/controllers/Builder.Controller.js
@@ -20,4 +20,32 @@ function BuilderController($scope, $rootScope, $routeParams, $miscUtils, $locati
     $scope.add_item = function (item) {
         //add item.backbone_model contains the survey representing the question
     }
+
+    /*jshint validthis: true */
+    var surveyDraftApi = $restApi.createSurveyDraftApi(scope.routeParams.id);
+
+    function saveCallback() {
+        if (this.validateSurvey()) {
+            surveyDraftApi.save({
+                    body: this.survey.toCSV(),
+                    description: this.survey.get('description'),
+                    name: this.survey.settings.get('form_title')
+                }, function () {
+                    $rootScope.deregisterLocationChangeStart && $rootScope.deregisterLocationChangeStart()
+                    $(window).unbind('beforeunload');
+                    $routeTo.forms()
+                });
+        }
+    }
+
+    if (scope.routeParams.id && scope.routeParams.id != 'new'){
+        surveyDraftApi.get({id: scope.routeParams.id}, function builder_get_callback(response) {
+            scope.xlfSurvey = XLF.createSurveyFromCsv(response.body);
+            // temporarily saving response in __djangoModelDetails
+            scope.xlfSurvey.__djangoModelDetails = response;
+            new SurveyApp({el: element, survey: scope.xlfSurvey, save: saveCallback}).render();
+        });
+    } else {
+        new SurveyApp({el: element, survey: scope.xlfSurvey, save: saveCallback}).render();
+    }
 }
--- dkobo/koboform/static/js/directives/Builder.Directive.js
+++ dkobo/koboform/static/js/directives/Builder.Directive.js
@@ -7,33 +7,7 @@
 function BuilderDirective($rootScope, $restApi, $routeTo) {
     return {
         link: function (scope, element) {
-/*jshint validthis: true */
-            var surveyDraftApi = $restApi.createSurveyDraftApi(scope.routeParams.id);
 
-            function saveCallback() {
-                if (this.validateSurvey()) {
-                    surveyDraftApi.save({
-                            body: this.survey.toCSV(),
-                            description: this.survey.get('description'),
-                            name: this.survey.settings.get('form_title')
-                        }, function () {
-                            $rootScope.deregisterLocationChangeStart && $rootScope.deregisterLocationChangeStart()
-                            $(window).unbind('beforeunload');
-                            $routeTo.forms()
-                        });
-                }
-            }
-
-            if (scope.routeParams.id && scope.routeParams.id != 'new'){
-                surveyDraftApi.get({id: scope.routeParams.id}, function builder_get_callback(response) {
-                    scope.xlfSurvey = XLF.createSurveyFromCsv(response.body);
-                    // temporarily saving response in __djangoModelDetails
-                    scope.xlfSurvey.__djangoModelDetails = response;
-                    new SurveyApp({el: element, survey: scope.xlfSurvey, save: saveCallback}).render();
-                });
-            } else {
-                new SurveyApp({el: element, survey: scope.xlfSurvey, save: saveCallback}).render();
-            }
         }
     };
 }
