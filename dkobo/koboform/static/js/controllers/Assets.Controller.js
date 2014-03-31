d5c59edaea49c78498ce9e69e73de870029c1a88
Alex Dorey <dorey415@gmail.com>
2014-03-31 09:54:40 -0700

    handling cases where a backbone_survey has no rows

--- dkobo/koboform/static/js/controllers/Assets.Controller.js
+++ dkobo/koboform/static/js/controllers/Assets.Controller.js
@@ -4,10 +4,10 @@ function AssetsController($scope, $rootScope, $resource, $restApi) {
     var assets = $restApi.create_question_api();
     $scope.sort_criteria = '-date';
 
-    assets.query(function (results) {
-
-        for (var i = 0; i < results.length; i++) {
-            var current = results[i];
+    assets.query(function (inputs) {
+        var results = [];
+        for (var i = 0; i < inputs.length; i++) {
+            var current = inputs[i];
             current.meta = {
                 show_responses: false,
                 is_selected: false,
@@ -20,23 +20,27 @@ function AssetsController($scope, $rootScope, $resource, $restApi) {
             var backbone_survey = XLF.createSurveyFromCsv(current.body);
             var row = backbone_survey.rows.at(0);
 
-            current.label = row.getValue('label');
-            current.type = row.get("type").get("typeId");
-            current.backbone_model = backbone_survey;
+            if(!row) {
+                // log("Why is there no row here?", backbone_survey);
+            } else {
+                current.label = row.getValue('label');
+                current.type = row.get("type").get("typeId");
+                current.backbone_model = backbone_survey;
 
-            var list = row.getList();
-            if (list) {
-                current.responses = list.options.map(function(option) {
-                    return option.get("label");
-                });
+                var list = row.getList();
+                if (list) {
+                    current.responses = list.options.map(function(option) {
+                        return option.get("label");
+                    });
+                }
+                results.push(row);
+
+                // for demo purposes
+                row.date = new Date().setDate(new Date().getDate() - i);
             }
 
-
-            // for demo purposes
-
-            results[i].date = new Date().setDate(new Date().getDate() - i);
         }
-        $scope.info_list_items = results;
+        $scope.info_list_items = inputs;
     });
 
     $scope.toggle_response_list = function (item) {
