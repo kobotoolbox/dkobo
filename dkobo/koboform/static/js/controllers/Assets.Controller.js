ea7cd9e18afdf5a67724e4f3221117ba7a951936
Nicolas Straub <nicostraub@gmail.com>
2014-04-01 14:29:02 -0300

    search bug and backbone model deffered creation

--- dkobo/koboform/static/js/controllers/Assets.Controller.js
+++ dkobo/koboform/static/js/controllers/Assets.Controller.js
@@ -1,37 +1,42 @@
 /*exported AssetsController*/
+/*global XLF*/
+/*global _*/
 'use strict';
-function AssetsController($scope, $rootScope, $resource, $restApi) {
+function AssetsController($scope, $rootScope, $resource, $restApi, $timeout) {
     var assets = $restApi.create_question_api();
-    $scope.sort_criteria = '-date_modified';
-    $scope.showImportButton = false;
-    $scope.showCreateButton = false;
+    $scope.sort_criteria = 'date_modified';
+    $rootScope.showImportButton = false;
+    $rootScope.showCreateButton = false;
+    $scope.filters = {};
 
     assets.query(function (inputs) {
-        var results = [];
-        for (var i = 0; i < inputs.length; i++) {
-            var current = inputs[i];
-            current.meta = {
+        $scope.info_list_items = inputs;
+
+        function create_survey(item) {
+            item.backbone_model = XLF.createSurveyFromCsv(item.body);
+        }
+
+        function set_defaults(item) {
+            item.meta = {
                 show_responses: false,
                 is_selected: false,
                 question_class: 'questions__question',
                 question_type_class: 'question__type',
                 question_type_icon: 'fa fa-caret-right',
                 question_type_icon_class: 'question__type-icon'
-            }
+            };
+        }
 
-            var backbone_survey = XLF.createSurveyFromCsv(current.body);
-            var row = backbone_survey.rows.at(0);
+        function get_props_from_row(item) {
+            var row = item.backbone_model.rows.at(0);
 
-            if(!row) {
-                // log("Why is there no row here?", backbone_survey);
-            } else {
-                current.label = row.getValue('label');
-                current.type = row.get("type").get("typeId");
-                current.backbone_model = backbone_survey;
+            if(row) {
+                item.label = row.getValue('label');
+                item.type = row.get("type").get("typeId");
 
                 var list = row.getList();
                 if (list) {
-                    current.responses = list.options.map(function(option) {
+                    item.responses = list.options.map(function(option) {
                         return option.get("label");
                     });
                 }
@@ -40,9 +45,29 @@ function AssetsController($scope, $rootScope, $resource, $restApi) {
                 // for demo purposes
                 row.date = new Date().setDate(new Date().getDate() - i);
             }
-
         }
-        $scope.info_list_items = inputs;
+
+        var results = [];
+        var i = 0;
+
+
+        function timed_execution(item) {
+            return $timeout(function () {
+                set_defaults(item);
+            }, 50).then(function () {
+                create_survey(item);
+            }).then(function () {
+                get_props_from_row(item);
+            }).then(function () {
+                i++;
+                if (i < $scope.info_list_items.length) {
+                    timed_execution($scope.info_list_items[i]);
+                }
+            });
+        }
+
+        timed_execution($scope.info_list_items[i]);
+
     });
 
     $scope.toggle_response_list = function (item) {
@@ -52,16 +77,16 @@ function AssetsController($scope, $rootScope, $resource, $restApi) {
 
         if (item.meta.show_responses) {
             item.meta.show_responses = false;
-            item.meta.question_type_class = 'question__type'
-            item.meta.question_type_icon = 'fa fa-caret-right'
-            item.meta.question_type_icon_class = 'question__type-icon'
+            item.meta.question_type_class = 'question__type';
+            item.meta.question_type_icon = 'fa fa-caret-right';
+            item.meta.question_type_icon_class = 'question__type-icon';
         } else {
             item.meta.question_type_class = 'question__type question__type--expanded';
             item.meta.question_type_icon_class = 'question__type-icon question__type--expanded-icon';
             item.meta.question_type_icon = 'fa fa-caret-down';
             item.meta.show_responses = true;
         }
-    }
+    };
 
     $scope.select_all = null;
 
@@ -89,15 +114,15 @@ function AssetsController($scope, $rootScope, $resource, $restApi) {
 
     $scope.get_selected_amount = function () {
         var amount = _.filter($scope.info_list_items, function (item) {
-            return item.meta.is_selected;
-        }).length
+            return item.meta ? item.meta.is_selected : false;
+        }).length;
 
         if (amount > 1 || amount === 0) {
             return amount + ' questions';
         } else {
             return amount + ' question';
         }
-    }
+    };
 
     $rootScope.canAddNew = true;
     $rootScope.activeTab = 'Question Library';
