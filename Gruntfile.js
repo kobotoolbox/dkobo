9ec375fb6b9a7eab3348ead97511ad79012c6c8c
Alex Dorey <dorey415@gmail.com>
2014-04-22 09:16:39 -0700

    gruntfile plus karma.conf changes. live reload on css changes. travis file updated to reflect new `grunt test` command

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,56 +1,138 @@
 module.exports = function(grunt) {
-  var _ = require('underscore');
+    var _ = require('underscore');
 
-  var stubbedPaths = {
-    'backbone': 'build_stubs/backbone',
-    'underscore': 'build_stubs/underscore',
-  };
-  var paths = {
-    'almond': 'components/almond/almond',
-    'jquery': 'components/jquery/dist/jquery.min',
-    'cs' :'components/require-cs/cs',
-    'backbone': 'components/backbone/backbone',
-    'underscore': 'components/underscore/underscore',
-    'coffee-script': 'components/require-cs/coffee-script',
-  };
-  var projectPaths = {
-    'xlform_model_view': 'xlform_model_view',
-  };
+    var stubbedPaths = {
+        'backbone': 'build_stubs/backbone',
+        'underscore': 'build_stubs/underscore',
+    };
+    var paths = {
+        'almond': 'components/almond/almond',
+        'jquery': 'components/jquery/dist/jquery.min',
+        'cs' :'components/require-cs/cs',
+        'backbone': 'components/backbone/backbone',
+        'underscore': 'components/underscore/underscore',
+        'coffee-script': 'components/require-cs/coffee-script',
+    };
+    var projectPaths = {
+        'xlform_model_view': 'xlform_model_view',
+    };
 
-  var mainConfigs = {
-    baseUrl: 'jsapp',
-    // uglify-minimization/optimization--
-    optimize: 'none',
-    stubModules: ['cs'],
-    paths: paths,
-    wrap: true,
-    exclude: ['coffee-script'],
-  };
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            /** changes to the source files trigger a karma retest
+             */
+            sourceChanged: {
+                files: ['jsapp/kobo/**/*.js', 'jsapp/kobo/**/*.coffee', 'jsapp/kobo/**/*.html'],
+                tasks: ['karma:unit:run'],
+            },
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    requirejs: {
-      compile_xlform: {
-        options: _.extend({}, mainConfigs, {
-          name: 'almond',
-          include: 'build_configs/dkobo_xlform',
-          out: 'jsapp/run/dkobo_xlform.js',
-          paths: _.extend({}, paths, projectPaths, stubbedPaths),
-        })
-      },
-    },
-    watch: {
-      files: ['jsapp/**/*.js', 'jsapp/**/*.coffee'],
-      tasks: ['requirejs'],
-      options: {
-        spawn: false,
-      },
-    }
-  });
+            /** changes to the tests trigger a karma retest
+             */
+            testsChanged: {
+                files: [
+                    // do we want to jump to all coffee tests?
+                    'jsapp/test/**/*.js',
+                    'jsapp/test/**/*.coffee'
+                ],
+                tasks: ['karma:unit:run'],
+            },
 
-  grunt.loadNpmTasks('grunt-contrib-requirejs');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-sass');
+            /** dkobo_xlform.js is build with and AMD packaging module
+             *    and is referenced by python and browser.
+             *
+             *  Changes in the source directory should rebuild the file, which ends up
+             *    eventually triggering 'sourceChanged' as well.
+             */
+            rebuildDkoboXlform: {
+                files: ['jsapp/xlform_model_view/**/*.js', 'jsapp/xlform_model_view/**/*.coffee'],
+                tasks: ['requirejs:compile_xlform'],
+            },
 
-  grunt.registerTask('default', ['watch',]);
+            /** One of the scss files changed, which triggers a rebuild
+             *  of the generated css files.
+             */
+            scssChanged: {
+                files: ['jsapp/**/*.scss'],
+                tasks: ['sass:dist'],
+                options: { spawn: false },
+            },
+
+            cssChanged: {
+                files: ['jsapp/**/*.css'],
+                options: { livereload: true },
+            }
+        },
+        karma: {
+            unit: {
+                configFile: 'jsapp/test/configs/karma.conf.js',
+                singleRun: true,
+                browsers: ['PhantomJS'],
+            },
+            travis: {
+                configFile: 'jsapp/test/configs/karma.conf.js',
+                singleRun: true,
+                browsers: ['PhantomJS'],
+            },
+        },
+
+        requirejs: {
+            compile_xlform: {
+                options: {
+                    baseUrl: 'jsapp',
+                    // uglify-minimization/optimization--
+                    optimize: 'none',
+                    stubModules: ['cs'],
+                    paths: paths,
+                    wrap: true,
+                    exclude: ['coffee-script'],
+                    name: 'almond',
+                    include: 'build_configs/dkobo_xlform',
+                    out: 'jsapp/kobo.compiled/dkobo_xlform.js',
+                    paths: _.extend({}, paths, projectPaths, stubbedPaths),
+                },
+            },
+        },
+
+        sass: {
+            dist: {
+                options: {
+                    style: 'expanded'
+                },
+                files: {
+                    /*
+                    'destinationFile.css' :          'originSourceFile.scss'
+                    */
+                    'jsapp/kobo.compiled/kobo.css' : 'jsapp/kobo/kobo.scss',
+                },
+            },
+        },
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+
+    grunt.registerTask('build', [
+        'requirejs:compile_xlform',
+        'sass:dist'
+    ]);
+
+    grunt.registerTask('test', ['karma:travis']);
+    grunt.registerTask('default', [
+        'requirejs:compile_xlform',
+        'sass:dist',
+        'watch',
+    ]);
 };
+
+/*
+http://stackoverflow.com/questions/22319397/running-yeoman-angular-generator-karma-dependency-error
+npm install karma@0.11.14 grunt-karma@0.7.2
+
+'karma-junit-reporter',
+'karma-chrome-launcher',
+'karma-firefox-launcher',
+'karma-phantomjs-launcher',
+*/
--- jsapp/test/configs/karma.conf.js
+++ jsapp/test/configs/karma.conf.js
@@ -4,29 +4,83 @@ module.exports = function(config) {
     config.set({
         basePath: path.resolve(__dirname, '../..'),
         frameworks: ['jasmine'],
-        files: [],
-        exclude: [],
+        files: [
+            // angular
+            'components/angular/angular.js',
+            // formerly: '../dkobo/static/js/angular-*.js',
+            'components/angular-cookies/angular-cookies.js',
+            'components/angular-mocks/angular-mocks.js',
+            'components/angular-resource/angular-resource.js',
+            'components/angular-route/angular-route.js',
+
+            // formerly contents of 'sut/lib/*.js',
+            'components/sinonjs/sinon.js',
+            'components/jasmine-sinon/lib/jasmine-sinon.js',
+            'components/underscore/underscore.js',
+            'components/backbone/backbone.js',
+
+            // kobo jsapp source files
+            'kobo/**/*.js',
+            'kobo/**/*.coffee',
+            'kobo/**/*.html',
+
+            // kobo jsapp compiled files
+            'kobo.compiled/**/*.js',
+            'kobo.compiled/**/*.html',
+
+            // jsapp/test files
+            'test/**/*.js',
+            'test/**/*.coffee',
+        ],
+        plugins: [
+            // jasmine + reporters
+            'karma-jasmine',
+            'karma-coverage',
+            'karma-junit-reporter',
+            'karma-growl-reporter',
+
+            // browser launchers
+            'karma-phantomjs-launcher',
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+
+            // preprocessors
+            'karma-coffee-preprocessor',
+            'karma-ng-html2js-preprocessor',
+        ],
+        // exclude: [],
         // test results reporters: (dots|progress|junit|growl|coverage)
         reporters: ['progress', 'coverage'],
         preprocessors: {
-          '../*/*.js': ['coverage'],
-          '../../templates/*.html': 'ng-html2js',
-          '../**/*.coffee': 'coffee'
+          // '../**/*.js': ['coverage'],
+          '**/*.html': 'ng-html2js',
+          '**/*.coffee': ['coffee'],
+        },
+        coffeePreprocessor: {
+            options: {
+                bare: true,
+                sourceMap: false
+            },
+            transformPath: function(path) {
+                return path.replace(/\.coffee$/, '.karma.js');
+            },
         },
         /// https://github.com/vojtajina/ng-directive-testing/commit/7b7a0b8f6b3698868daddc40828da39c3c6b6272#diff-766793014309586429b517112184567d
         /// https://github.com/karma-runner/karma-ng-html2js-preprocessor
         ngHtml2JsPreprocessor: {
             cacheIdFromPath: function(filepath) {
-                var matches = /^\/(.+\/)*(.+)\.(.+)$/.exec(filepath);
+                var matches = /^\/?(.+\/)*(.+)\.(.+)$/.exec(filepath);
                 return 'templates/' + matches[2] + '.' + matches[3];
             }
         },
+
         port: 9876,
         colors: true,
         logLevel: config.LOG_INFO,
         // enable / disable watching file and executing tests whenever any file changes
         autoWatch: true,
-        browsers: ['Chrome', 'PhantomJS'],
+        // browsers: ['PhantomJS', 'Chrome'],
+        browsers: ['PhantomJS'],
         captureTimeout: 60000,
         singleRun: false,
     });
