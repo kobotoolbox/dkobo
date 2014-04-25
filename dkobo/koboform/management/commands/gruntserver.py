6d9e8e089d27da6e7d474f39581ea1a1aeb16a75
Alex Dorey <dorey415@gmail.com>
2014-04-25 08:56:46 -0400

    gruntserver

---
+++ dkobo/koboform/management/commands/gruntserver.py
@@ -0,0 +1,34 @@
+import os
+import subprocess
+import atexit
+import signal
+
+from django.conf import settings
+from django.contrib.staticfiles.management.commands.runserver import Command\
+    as StaticfilesRunserverCommand
+
+class Command(StaticfilesRunserverCommand):
+    # As described in this blog post:
+    # http://lincolnloop.com/blog/simplifying-your-django-frontend-tasks-grunt/
+
+    def inner_run(self, *args, **options):
+        self.start_grunt()
+        return super(Command, self).inner_run(*args, **options)
+
+    def start_grunt(self):
+        self.stdout.write('>>> Starting grunt')
+        self.grunt_process = subprocess.Popen(
+            ['grunt --gruntfile={0}/Gruntfile.js --base=.'.format(settings.PROJECT_PATH)],
+            shell=True,
+            stdin=subprocess.PIPE,
+            stdout=self.stdout,
+            stderr=self.stderr,
+        )
+
+        self.stdout.write('>>> Grunt process on pid {0}'.format(self.grunt_process.pid))
+
+        def kill_grunt_process(pid):
+            self.stdout.write('>>> Closing grunt process')
+            os.kill(pid, signal.SIGTERM)
+
+        atexit.register(kill_grunt_process, self.grunt_process.pid)
