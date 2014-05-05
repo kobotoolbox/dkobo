3d23c06dc0c94535f2f0f14a77b0538a080c3d08
Alex Dorey <dorey415@gmail.com>
2014-05-05 10:26:35 -0700

    got sandbox working for form_builder page

--- dkobo/koboform/views.py
+++ dkobo/koboform/views.py
@@ -160,8 +160,8 @@ def survey_draft_detail(request, pk, format=None):
 def jasmine_spec(request):
     context = RequestContext(request)
     context['DEBUG'] = settings.DEBUG
-    context['run_jasmine'] = True
-    context['include_coffeefile'] = "test/unit/SkipLogic.Tests.coffee"
+    context['run_jasmine'] = False
+    context['include_coffeefile'] = "kobo/stylesheets/pages/form_builder.coffee"
     # context['include_js'] = "test/unit/Validator.Tests.js"
     return render_to_response("jasmine_spec.html", context_instance=context)
 
