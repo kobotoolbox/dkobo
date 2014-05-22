a570cd0a742d249a1ac97975b779593a8e83184e
Alex Dorey <dorey415@gmail.com>
2014-05-22 12:07:12 -0700

    changes to scripts to get things working in dev and in production

--- scripts/collectstatic.sh
+++ scripts/collectstatic.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 python manage.py collectstatic --noinput --settings=dkobo.settings
 grunt build_all
+npm install yuglify
 python manage.py compress --settings=dkobo.settings
 mkdir -p jsapp/CACHE
 cp -R jsapp/components/fontawesome/fonts jsapp/CACHE/fonts
