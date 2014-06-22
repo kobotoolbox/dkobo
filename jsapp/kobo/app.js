4d963fd00d1839e475d034d79607318e66c868af
pmusaraj <pmusaraj@gmail.com>
2014-06-21 22:16:34 -0400

    added poshytip styling to form settings help icons and forms list publish button; added dropdown to Add Form in forms list

--- jsapp/kobo/app.js
+++ jsapp/kobo/app.js
@@ -87,6 +87,26 @@ kobo.run(function ($http, $cookies, $miscUtils) {
             autoOpen: false,
             modal: true
         });
+
+        // forms__list poshytip effect on publish button
+        $('.forms__poshytip').poshytip({
+            className: 'tip__rightarrow',
+            showTimeout: 1,
+            alignTo: 'target',
+            offsetX: 10,
+            offsetY: -16,
+            liveEvents: true
+        });
+
+        // question mark poshytip effect (in form__settings)
+        $('span.poshytip').poshytip({
+            className: 'tip__bottomarrow',
+            showTimeout: 1,
+            alignTo: 'target',
+            alignX: 'right',
+            alignY: 'inner-bottom',
+            liveEvents: true
+        });
     });
     // jQuery.fileupload for importing forms to the user's form list.
 //    $miscUtils.bootstrapFileUploader();
