e8f5937d4be7a93684aedf89686fdbabd7cef1a0
nstraub <nicostraub@gmail.com>
2014-07-01 18:55:57 -0400

    #960 - On formlist screen, dropdown boxes don't close

--- jsapp/kobo/directives/OutsideClick.Directive.js
+++ jsapp/kobo/directives/OutsideClick.Directive.js
@@ -1,13 +1,21 @@
 /// http://vadimpopa.com/onblur-like-for-a-div-in-angularjs-to-close-a-popup/
 function OutsideClickDirective ( $document ){
     return {
-        link: function( $scope, $element, $attributes ){
+        scope: {
+            isShowing: '=',
+            closeMethod: '&'
+        },
+        link: function( scope, $element, $attributes ) {
             var scopeExpression = $attributes.outsideClick,
                 onDocumentClick = function(event){
                     var isChild = $element.find(event.target).length > 0;
 
-                    if(!isChild) {
-                        $scope.$apply(scopeExpression);
+                    if (!scope.isShowing) {
+                        if (!isChild) {
+                            scope.closeMethod();
+                        }
+                    } else {
+                        scope.isShowing = false;
                     }
                 };
 
