e7cec95be641ca3370e932c859969db3379a6146
Alex Dorey <dorey415@gmail.com>
2014-09-12 12:03:42 -0400

    adding a message with the date and time of latest deployment

--- scripts/collectstatic.sh
+++ scripts/collectstatic.sh
@@ -3,6 +3,7 @@ python manage.py collectstatic --noinput --settings=dkobo.settings
 grunt build_all
 npm install yuglify
 python manage.py compress --settings=dkobo.settings
+echo "Deployed: $(date)" > dkobo/static/deployed.txt
 mkdir -p jsapp/CACHE
 cp -R jsapp/components/fontawesome/fonts jsapp/CACHE/fonts
 python manage.py collectstatic --noinput --settings=dkobo.settings
