6962db718974bc34b737941bc03b4e03181edf52
Alex Dorey <dorey415@gmail.com>
2014-09-19 12:36:46 -0400

    fixing issue where get_current_site returns empty domain

--- dkobo/main/context_processors.py
+++ dkobo/main/context_processors.py
@@ -1,11 +1,13 @@
 from dkobo.main.models import SitewideMessages
-from django.contrib.sites.models import get_current_site
+from django.contrib.sites.models import Site
+from django.conf import settings
 
 def welcome_message(request):
     if request.path == '/accounts/register/':
         try:
             w_message = SitewideMessages.objects.get(slug='welcome_message')
-            return {'welcome_message': w_message.body, 'site_domain': get_current_site(request).domain}
+            domain = Site.objects.get(id=settings.SITE_ID).domain
+            return {'welcome_message': w_message.body, 'site_domain': domain}
         except SitewideMessages.DoesNotExist, e:
             pass
     return {}
