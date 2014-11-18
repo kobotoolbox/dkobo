047f78e20e29ae948fec79eb78a037afd49103b0
Nicolas Straub <spameggspambakedbeansandspam@gmail.com>
2014-11-18 12:41:52 -0300

    fixes regression from merge

--- jsapp/kobo/controllers/Builder.Controller.js
+++ jsapp/kobo/controllers/Builder.Controller.js
@@ -41,7 +41,7 @@ function BuilderController($scope, $rootScope, $routeParams, $routeTo, $miscUtil
                 throw e;
             }
             return surveyDraftApi.save({
-                id: $scope.routeParams.id,
+                id: $scope.routeParams.id !== 'new' && $scope.routeParams.id,
                 body: survey,
                 description: this.survey.get('description'),
                 name: this.survey.settings.get('form_title')
