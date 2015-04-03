ff188bfc30d84bfc65a911da5a7f0c51e2ea9fd0
Alex Dorey <dorey415@gmail.com>
2015-04-03 16:02:18 -0400

    making sitewide messages usable in different contexts
    [skip ci]

--- dkobo/hub/context_processors.py
+++ dkobo/hub/context_processors.py
@@ -1,24 +1,50 @@
 from dkobo.hub.models import SitewideMessage
 from django.contrib.sites.models import Site
+import re
 from django.conf import settings
 from dkobo.koboform import kobocat_integration
 
 def welcome_message(request):
-    if request.path == '/accounts/register/':
-        ctx = {}
-        try:
-            w_message = SitewideMessage.objects.get(slug='welcome_message')
-            ctx['welcome_message'] = w_message.body
-        except SitewideMessage.DoesNotExist, e:
-            pass
+    print """
+    please update dkobo settings.py to use 'dkobo.hub.context_processors.site_messages'
+    instead of 'dkobo.hub.context_processors.welcome_message'
+    """
+    return site_messages(request)
 
-        if settings.KOBOCAT_URL:
-            ctx['kobocat_url'] = settings.KOBOCAT_URL
-        else:
-            ctx['kobocat_url'] = None
-        return ctx
-    else:
-        return {}
+def _unwrap_paragraph(msg):
+    """
+    markdown wraps all text in paragraphs.
+    this is great, but not always what we want.
+    this provides a way to 
+    """
+    matches = re.match(r'^<div nowrap>(.*)</div>$', msg)
+    if matches:
+        try:
+            return matches.groups()[0]
+        except KeyError:
+            return msg
+    return msg
+
+def site_messages(request):
+    ctx = {}
+    site_messages = [
+            'logo', # in the top bar. displayed in index.html
+            'head', # css
+            'footer', # in the footer bar
+            ]
+
+    if request.path == '/accounts/register/':
+        site_messages.append('welcome_message')
+
+    messages = SitewideMessage.objects.filter(slug__in=site_messages)
+
+    for message in messages:
+        template_accessible_slug = "sitewide__%s" % message.slug
+        ctx[template_accessible_slug] = _unwrap_paragraph(message.body)
+
+    if settings.KOBOCAT_URL:
+        ctx['kobocat_url'] = settings.KOBOCAT_URL
+    return ctx
 
 def external_service_tokens(request):
     context = {}
--- dkobo/hub/models.py
+++ dkobo/hub/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 from markitup.fields import MarkupField
+from django.contrib.sites.models import Site
 
 class SitewideMessage(models.Model):
     slug = models.CharField(max_length=50)
     body = MarkupField()
+    site = models.ForeignKey(Site, null=True)
+
     def __str__(self):
         return self.slug
