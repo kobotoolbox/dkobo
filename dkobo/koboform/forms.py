9cea6b1c823d38fd8e9e1a5834a122c171af7b6c
John N. Milner <john@tmoj.net>
2015-06-14 11:14:48 -0400

    Fix botched regex from 0526470d675bc4a55ce156747f41dd176ef55b52

--- dkobo/koboform/forms.py
+++ dkobo/koboform/forms.py
@@ -1,13 +1,13 @@
 from django import forms
-from django.utils.translation import ugettext_lazy as _                         
+from django.utils.translation import ugettext_lazy as _
 from registration import forms as registration_forms
 
-class RegistrationForm(registration_forms.RegistrationForm):                                             
+class RegistrationForm(registration_forms.RegistrationForm):
     username = forms.RegexField(
         # Should match dkobo/static/js/validator.js
-        regex=r'^[a-z][a-z0-9_]$',                           
-        max_length=30,                                  
-        label=_("Username"),                            
+        regex=r'^[a-z][a-z0-9_]+$',
+        max_length=30,
+        label=_("Username"),
         error_messages={'invalid': _(
             'A username may only contain lowercase letters, numbers, and underscores (_).'
         )}
