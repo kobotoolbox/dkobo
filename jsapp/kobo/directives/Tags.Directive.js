191d3ade297a306e8ef5f4a8be14808b04ba2d2c
John N. Milner <john@tmoj.net>
2015-09-23 15:56:24 -0400

    Revert "#1639 - Question Library Question tag editor doesn't work if tags are empty array"
    This reverts commit 157ba5c21a3ee4cc428d502571186b23c995b7e7.

--- jsapp/kobo/directives/Tags.Directive.js
+++ jsapp/kobo/directives/Tags.Directive.js
@@ -6,20 +6,22 @@ kobo.directive ('tags', function () {
         link: function (scope,element) {
 
             var clearSourceWatch = scope.$watch('model.available.length', function () {
-                element.val(scope.model.selected);
-                element.tagsInput({
-                    width: '700px',
-                    height: '45px',
-                    autocomplete_url: 'na', //this needs to be !=undefined for autocomplete to work
-                    autocomplete: {
-                        source: _.sortBy(scope.model.available, function (name){return name.toLowerCase(); }),
-                        minLength: 0
-                    },
-                    onChange: function () {
-                        scope.model.selected = $(this).val();
-                    }
-                });
-                clearSourceWatch();
+                if (scope.model.available.length > 0) {
+                    element.val(scope.model.selected);
+                    element.tagsInput({
+                        width: '700px',
+                        height: '45px',
+                        autocomplete_url: 'na', //this needs to be !=undefined for autocomplete to work
+                        autocomplete: {
+                            source: _.sortBy(scope.model.available, function (name){return name.toLowerCase(); }),
+                            minLength: 0
+                        },
+                        onChange: function () {
+                            scope.model.selected = $(this).val();
+                        }
+                    });
+                    clearSourceWatch();
+                }
             })
         }
     }
