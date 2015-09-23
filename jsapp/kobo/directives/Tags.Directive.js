ed29ea8b8d640ba10c5ad268a37114d5567c4009
John N. Milner <john@tmoj.net>
2015-09-23 16:13:13 -0400

    Make tags input for library item behave properly. Fixes #594; see also #534

--- jsapp/kobo/directives/Tags.Directive.js
+++ jsapp/kobo/directives/Tags.Directive.js
@@ -4,22 +4,27 @@ kobo.directive ('tags', function () {
             model: '='
         },
         link: function (scope,element) {
+            // Transform the plain text input into a tags input box now, even
+            // though the existing tags might not be loaded yet
+            element.tagsInput({
+                width: '700px',
+                height: '45px',
+                autocomplete_url: 'na', //this needs to be !=undefined for autocomplete to work
+                autocomplete: {
+                    source: _.sortBy(scope.model.available, function (name){return name.toLowerCase(); }),
+                    minLength: 0
+                },
+                onChange: function () {
+                    scope.model.selected = $(this).val();
+                }
+            });
 
+            // Watch for the existing tags to be loaded
             var clearSourceWatch = scope.$watch('model.available.length', function () {
                 if (scope.model.available.length > 0) {
-                    element.val(scope.model.selected);
-                    element.tagsInput({
-                        width: '700px',
-                        height: '45px',
-                        autocomplete_url: 'na', //this needs to be !=undefined for autocomplete to work
-                        autocomplete: {
-                            source: _.sortBy(scope.model.available, function (name){return name.toLowerCase(); }),
-                            minLength: 0
-                        },
-                        onChange: function () {
-                            scope.model.selected = $(this).val();
-                        }
-                    });
+                    // The existing tags have been received! Import them into
+                    // the tags input element and stop watching
+                    element.importTags(scope.model.selected);
                     clearSourceWatch();
                 }
             })
