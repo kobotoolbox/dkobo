26697e230226bf9092cc33fc6dce7694a453c1a7
John N. Milner <john@tmoj.net>
2015-09-24 23:23:32 -0400

    Compatibility with kpi's native migrations

--- dkobo/hub/migrations/0002_formbuilderpreference.py
+++ dkobo/hub/migrations/0002_formbuilderpreference.py
@@ -1,27 +1,92 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models, ProgrammingError
 
-from django.db import models, migrations
-from django.conf import settings
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'FormBuilderPreference'
+        db.create_table(u'hub_formbuilderpreference', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('user', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['auth.User'], unique=True)),
+            ('preferred_builder', self.gf('django.db.models.fields.CharField')(default='D', max_length=1)),
+        ))
+        db.send_create_signal(u'hub', ['FormBuilderPreference'])
+        try:
+            db.execute(
+                "INSERT INTO django_migrations (app, name, applied) " \
+                "VALUES ('hub', '0002_formbuilderpreference', NOW());"
+            )
+        except ProgrammingError:
+            # Django >= 1.7 may nave never touched this database
+            pass
 
 
-class Migration(migrations.Migration):
+    def backwards(self, orm):
+        # Deleting model 'FormBuilderPreference'
+        db.delete_table(u'hub_formbuilderpreference')
+        try:
+            db.execute(
+                "DELETE FROM django_migrations WHERE app='hub' AND " \
+                "name='0002_formbuilderpreference';"
+            )
+        except ProgrammingError:
+            pass
 
-    dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('hub', '0001_initial'),
-    ]
 
-    operations = [
-        migrations.CreateModel(
-            name='FormBuilderPreference',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('preferred_builder', models.CharField(default=b'D', max_length=1, choices=[(b'K', b'kpi'), (b'D', b'dkobo')])),
-                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
-            ],
-            options={
-            },
-            bases=(models.Model,),
-        ),
-    ]
+    models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'hub.formbuilderpreference': {
+            'Meta': {'object_name': 'FormBuilderPreference'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'preferred_builder': ('django.db.models.fields.CharField', [], {'default': "'D'", 'max_length': '1'}),
+            'user': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['auth.User']", 'unique': 'True'})
+        },
+        u'hub.sitewidemessage': {
+            'Meta': {'object_name': 'SitewideMessage'},
+            u'_body_rendered': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'body': ('markitup.fields.MarkupField', [], {u'no_rendered_field': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'slug': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        }
+    }
+
+    complete_apps = ['hub']
