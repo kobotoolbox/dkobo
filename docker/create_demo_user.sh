48da4c2654376489beed325ed538f46d1c19205e
Esmail Fadae <esmail.fadae@kobotoolbox.org>
2016-02-04 17:54:48 -0500

    Docker-friendly defaults.

--- docker/create_demo_user.sh
+++ docker/create_demo_user.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
+set -e
 
-[ -z "$DEFAULT_KOBO_USER" ] && exit 1
-
-[ -z "$DEFAULT_KOBO_PASS" ] && exit 2
+DEFAULT_KOBO_USER=${DEFAULT_KOBO_USER:-kobo}
+DEFAULT_KOBO_PASS=${DEFAULT_KOBO_PASS:-kobo}
 
 cd /srv/src/koboform
 
 echo "from django.contrib.auth.models import User; print 'UserExists' if User.objects.filter(username='$DEFAULT_KOBO_USER').count() > 0 else User.objects.create_superuser('$DEFAULT_KOBO_USER', 'kobo@example.com', '$DEFAULT_KOBO_PASS');" \
     | python manage.py shell 2>&1
-
-
