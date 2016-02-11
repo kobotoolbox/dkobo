64cf374663af41e1948f447f94c70e823d7796b5
Esmail Fadae <esmail.fadae@kobotoolbox.org>
2016-02-11 17:17:55 -0500

    Wait for Postgres.

--- docker/init.sh
+++ docker/init.sh
@@ -6,14 +6,29 @@ export PATH=$PATH:./node_modules/.bin
 oldpwd=$(pwd)
 cd /srv/src/koboform
 
+echo 'Waiting for Postgres.'
+KOBO_PSQL_DB_NAME=${KOBO_PSQL_DB_NAME:-"kobotoolbox"}
+KOBO_PSQL_DB_USER=${KOBO_PSQL_DB_USER:-"kobo"}
+KOBO_PSQL_DB_PASS=${KOBO_PSQL_DB_PASS:-"kobo"}
+dockerize -timeout=20s -wait ${PSQL_PORT}
+until $(PGPASSWORD="${KOBO_PSQL_DB_PASS}" psql -d ${KOBO_PSQL_DB_NAME} -h psql -U ${KOBO_PSQL_DB_USER} -c '' 2> /dev/null); do
+    sleep 1
+done
+echo 'Postgres ready.'
+
+echo 'Synchronizing database.'
 python manage.py syncdb --noinput
 
 # FIXME: Convince South that KPI has already done the overlapping migrations.
-python manage.py migrate --fake hub
-python manage.py migrate --fake authtoken
-python manage.py migrate --fake taggit
-python manage.py migrate --fake reversion
+echo 'Running fake migrations.'
+python manage.py migrate --noinput --fake hub
+python manage.py migrate --noinput --fake authtoken
+python manage.py migrate --noinput --fake taggit
+python manage.py migrate --noinput --fake reversion
 
+echo 'Running migrations.'
 python manage.py migrate --noinput
+
+echo '\`dkobo\` initialization completed.'
 
 cd $oldpwd
